e4dac4a3a9e3c7dcba12a4916c13d789412a6fb2
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-04-07 14:50:21 +0530

    Print Hello with name

--- src/Basics/Basics_Codes.java
+++ src/Basics/Basics_Codes.java
@@ -2,13 +2,19 @@ package Basics;
 
 public class Basics_Codes {
     public static void main(String[] args) {
+
+        // Print Hello World
         System.out.println("Hello World");
 
+        // Add Two numbers
         int a = 40;
         int b = 20;
-
-        int total = a +b*2;
+        int total = a+b;
         System.out.println("Total: " + total );
+
+        //Print Hello with name
+        String name = "John Snow";
+        System.out.println("Hello " + name);
 
     }
 }
