c181206703aeddd193d098a3fdc1f91dda969c55
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-04-08 23:12:35 +0530

    Data_Types

---
+++ src/Basics/Data_Types.java
@@ -0,0 +1,23 @@
+package Basics;
+
+public class Data_Types {
+    /*
+    Primitive Data Types
+    boolean
+    byte
+    char
+    short
+    int
+    long
+    float
+    double
+     */
+
+    /*
+    Non-Primitive (Reference) Data Types
+    String
+    Class
+    Object
+    Interface
+     */
+}
