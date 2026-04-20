f9c17ecf096d1e20fbb8b58e165fde61ef2771a5
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-04-21 03:00:26 +0530

    Add: Input Problem

---
+++ src/Problems/Input.java
@@ -0,0 +1,14 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System. in);
+        int a = scn.nextInt();
+
+        a = a + 10;
+
+        System.out.println(a);
+    }
+}
